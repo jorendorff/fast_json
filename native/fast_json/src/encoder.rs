7ec4db4d5668da26cac966f5cc766a0f6d458b4a
Jason Orendorff <jason.orendorff@gmail.com>
2016-12-30 17:17:10 -0600

    Support maps in the encoder.

--- native/fast_json/src/encoder.rs
+++ native/fast_json/src/encoder.rs
@@ -1,6 +1,7 @@
-use rustler::{NifDecoder, NifEncoder, NifEnv, NifTerm, NifResult};
+use rustler::{NifDecoder, NifEncoder, NifEnv, NifTerm, NifResult, NifError};
 use rustler::atom::{NifAtom, get_atom};
 use rustler::list::NifListIterator;
+use rustler::map::NifMapIterator;
 use json;
 use json::JsonValue;
 
@@ -22,18 +23,28 @@ fn term_to_json<'a>(env: &'a NifEnv, term: NifTerm) -> NifResult<JsonValue> {
         handle_float(env, number)
     } else if let Ok(number) = <i64 as NifDecoder>::decode(term) {
         handle_integer(env, number)
+    } else if let Ok(iter) = <NifMapIterator as NifDecoder>::decode(term) {
+        handle_map(env, iter)
     } else {
-        panic!("fail")
+        Err(NifError::BadArg)
     }
 }
 
-//fn handle_map(env: &NifEnv, iter: NifMapIterator) -> NifResult<JsonValue> {
-    //let values: NifResult<Vec<JsonValue>> = iter.map(|term| {
-        //term_to_json(env, term)
-    //}).collect();
-
-    //Ok(JsonValue::Array(try!(values)))
-//}
+fn handle_map(env: &NifEnv, iter: NifMapIterator) -> NifResult<JsonValue> {
+    let mut map = json::object::Object::new();
+    for (key, value) in iter {
+        let key_string =
+            if let Some(atom) = NifAtom::from_term(env, key) {
+                atom.to_string(env)
+            } else if let Ok(s) = <&str as NifDecoder>::decode(key) {
+                s.to_string()
+            } else {
+                return Err(NifError::BadArg);
+            };
+        map.insert(&key_string, term_to_json(env, value)?);
+    }
+    Ok(JsonValue::Object(map))
+}
 
 fn handle_list(env: &NifEnv, iter: NifListIterator) -> NifResult<JsonValue> {
     let values: NifResult<Vec<JsonValue>> = iter.map(|term| {
